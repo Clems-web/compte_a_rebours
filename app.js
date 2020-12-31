8e92beb865c06a17eedd971f96566916aee8679c
Clems-web <clement.servais1@gmail.com>
2020-12-31 13:44:06 +0100

    Presque finit (sans fct cronstructeur)

--- app.js
+++ app.js
@@ -1,29 +1,41 @@
 let buttonCrea = document.getElementById("buttonCreer");
 
 buttonCrea.addEventListener("click", ()=> {
+
     let heure = document.getElementById("heure").value;
     let minute = document.getElementById("minute").value;
     let seconde = document.getElementById("seconde").value;
     let divCrea = document.createElement("div");
     let buttonReset = document.createElement("button");
-
+    let buttonPause = document.createElement("button");
+    buttonPause.innerHTML = "Stop"
     buttonReset.className = "buttonReset";
     buttonReset.innerHTML = "Reset";
 
-    setInterval(function () {
+    let intervalId = setInterval(function () {
         divCrea.innerHTML = `${heure} : ${minute} : ${seconde}`;
         seconde--;
         if (seconde < 0) {
             minute--;
             seconde = 59;
-        }
+           }
         if (minute < 0 && heure > 0) {
             heure--;
             minute = 59;
+           }
+        if (heure === 0 && minute === 0 && seconde === 0) {
+            clearInterval(intervalId);
         }
-    },1000);
+        buttonPause.addEventListener("click", ()=> {
+            clearInterval(intervalId);
+        });
+        },1000);
 
     document.body.append(divCrea);
     document.body.append(buttonReset);
-
+    document.body.append(buttonPause);
 });
+
+
+
+
