aaaaf4cc52e49b434039b921d3d56dc2e7baa325
Clems-web <clement.servais1@gmail.com>
2021-01-02 16:49:45 +0100

    Presque finit (sans fct cronstructeur) j'y arrive po, aled

--- app.js
+++ app.js
@@ -6,33 +6,41 @@ buttonCrea.addEventListener("click", ()=> {
     let minute = document.getElementById("minute").value;
     let seconde = document.getElementById("seconde").value;
     let divCrea = document.createElement("div");
-    let buttonReset = document.createElement("button");
+    let buttonPlay = document.createElement("button");
     let buttonPause = document.createElement("button");
     buttonPause.innerHTML = "Stop"
-    buttonReset.className = "buttonReset";
-    buttonReset.innerHTML = "Reset";
+    buttonPlay.className = "buttonReset";
+    buttonPlay.innerHTML = "Play";
 
-    let intervalId = setInterval(function () {
+
+    let intervalId = setInterval(test, 1000);
+    function test() {
         divCrea.innerHTML = `${heure} : ${minute} : ${seconde}`;
         seconde--;
         if (seconde < 0) {
             minute--;
             seconde = 59;
-           }
+        }
         if (minute < 0 && heure > 0) {
             heure--;
             minute = 59;
-           }
+        }
         if (heure === 0 && minute === 0 && seconde === 0) {
             clearInterval(intervalId);
         }
         buttonPause.addEventListener("click", ()=> {
             clearInterval(intervalId);
         });
-        },1000);
+        buttonPlay.addEventListener("click", ()=> {
+            intervalId = setInterval(test, 1000);
+        });
+    }
+
+
+
 
     document.body.append(divCrea);
-    document.body.append(buttonReset);
+    document.body.append(buttonPlay);
     document.body.append(buttonPause);
 });
 
